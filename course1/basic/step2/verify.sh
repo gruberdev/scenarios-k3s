3931c667d7ac370a4228a662b27c0f8684d3d772
gruberdev <contact@gruber.dev.br>
2023-07-07 00:53:40 -0300

    updated verify script

--- course1/basic/step2/verify.sh
+++ course1/basic/step2/verify.sh
@@ -4,12 +4,12 @@
 KUBECTL='/usr/local/bin/k3s kubectl'
 
 # Retrieve the node information
-NODES=$($KUBECTL get nodes -o jsonpath='{range .items[*]}{.metadata.name} {.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}')
+NODES=$($KUBECTL get nodes -o jsonpath='{range .items[*]}{.metadata.name} {.status.conditions[?(@.type=="Ready")].status} {.status.nodeInfo.kubeletVersion} {.metadata.labels}{"\n"}{end}')
 
-# Check if the node status is "True"
-if [[ ${NODES[*]} == *"True"* ]]; then
-    echo "Kubernetes has been successfully created using K3s. Nodes are ready."
+# Check if the node status is "True", version is "v1.27.3", and node is control-plane
+if [[ ${NODES[*]} == *"True"* && ${NODES[*]} == *"v1.27.3"* && ${NODES[*]} == *"node-role.kubernetes.io/control-plane"* ]]; then
+    echo "Kubernetes has been successfully created using K3s. Nodes are ready, running version v1.27.3 and are control-plane nodes."
 else
-    echo "Kubernetes creation unsuccessful or Nodes are not ready."
+    echo "Kubernetes creation unsuccessful or Nodes are not ready, not running version v1.27.3, or are not control-plane nodes."
     exit 1
 fi
